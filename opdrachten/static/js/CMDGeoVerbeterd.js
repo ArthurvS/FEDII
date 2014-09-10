b32e0f6a46a50d73c1a3c8f59fd18df872065e53
arthur van schravendijk <arthur_vanschravendijk@hotmail.com>
2014-09-10 15:22:42 +0200

    CMDgeo script aanpassen

---
+++ opdrachten/static/js/CMDGeoVerbeterd.js
@@ -0,0 +1,25 @@
+(function(){
+	var gps = {
+		init: function () { },
+		startInterval: function () { },
+		updatePosition: function () { },
+		setPostion: function () { },
+		checkLocations: function () { },
+		calculateDistance: function () { }
+	}
+
+	var map = {
+		generate: function () { },
+		updatePosition: function () { }
+	}
+
+	var utils = {
+		isNumber: function() { }
+	}
+
+	var debug = {
+		message: function() { },
+		geoErrorHandler: function() { },
+		setCustomDebugging: function() { }
+	}
+})();
