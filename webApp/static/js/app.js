61aa39c757f78e0279e8ea2c7db07787c4edd9ef
unknown <arthur_the_king1993@hotmail.com>
2014-09-23 21:50:29 +0200

    finished DT 1: working router load content with tranparency.js add img

--- webApp/static/js/app.js
+++ webApp/static/js/app.js
@@ -1,24 +1,45 @@
-(function(){
+var CL4P = CL4P || { };
+window.onload = function(){
 
 	var controller = {
 		init:function () {
 			router.init();
+			sections.init();
 		}
-	}
-
+	};
 
 	var router = {
 		init: function () {
 			routie({
-	    		'about': function() {
-	    			console.log('Warp speed to about page');
+	    		about: function() {
+	    			sections.about();
+	    			sections.toggle(".about", ".movies");
 	    		},
-	    		'movies': function() {
-	    			console.log('Warp speed to movies page');
+	    		movies: function() {
+	    			sections.movies();
+	    			sections.toggle(".movies", ".about");
 	    		}
 			});
 		}
 	};
 
+	var sections = {
+		init: function () {
+			this.about();
+			this.movies();
+		},
+		about: function () {
+			Transparency.render(document.querySelector(".about"), CL4P.content.about, CL4P.aboutDirectives);
+		},
+		movies: function () {
+			Transparency.render(document.querySelector(".movies"), CL4P.content.movies, CL4P.movieDirectives);
+		},
+		toggle: function (show, hide) {
+			var show = document.querySelector(show);
+			var hide = document.querySelector(hide);
+			show.classList.add('active');
+			hide.classList.remove('active');			
+		} 
+	};
 	controller.init();
-}());
+};
