3ebcbaeb51c6a1bc61d954ef8884efcfa9985f3b
arthur van schravendijk <arthur_vanschravendijk@hotmail.com>
2014-10-26 22:43:29 +0100

    werkt weer

--- webApp/static/js/module.js
+++ webApp/static/js/module.js
@@ -1,10 +1,10 @@
 (function () {
-	var xhr = { bind: function (data) { postMessage(data);} };
-	var data = importScripts("http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/current_releases.json?page_limit=16&page=1&country=nl&apikey=5mz3wz9g8jfmgnbwt8a358nr&callback=xhr.bind");
-	// importScripts('vendor/xhr.js');
-	// self.xhr.trigger("get", "http://dennistel.nl/movies", function (e) {
-	// 			postMessage(e);	
-	// 		}, null);
+	// var xhr = { bind: function (data) { postMessage(data);} };
+	// var data = importScripts("http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/current_releases.json?page_limit=16&page=1&country=nl&apikey=5mz3wz9g8jfmgnbwt8a358nr&callback=xhr.bind");
+	importScripts('vendor/xhr.js');
+	self.xhr.trigger("get", "http://dennistel.nl/movies", function (e) {
+				postMessage(e);	
+			}, null);
 
 	// var script = document.createElement("script"); 
 	// script.src = 'http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/current_releases.json?page_limit=16&page=1&country=nl&apikey=5mz3wz9g8jfmgnbwt8a358nr&callback=APP.xhr.bind'; 
